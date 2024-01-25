ae470a492af32b323d67be170a67951c33b5de2d
Saurabh8734 <saurabhwabale8734@gmail.com>
2024-01-25 20:26:56 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -44,8 +44,10 @@ class DataIngestion:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
         
-
+from src.components.data_transformation import DataTransformation
 ## Data Ingestion
 if __name__=='__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
