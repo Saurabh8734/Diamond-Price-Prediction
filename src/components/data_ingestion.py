3422bf18f66a53c77d31b61b9a8abdb00b2b09ca
Saurabh8734 <saurabhwabale8734@gmail.com>
2024-01-25 22:00:50 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
 ##Intialize the Data Ingetion Configuration
 
@@ -44,10 +45,11 @@ class DataIngestion:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
         
-from src.components.data_transformation import DataTransformation
-## Data Ingestion
+"""
+##  Run Data Ingestion
 if __name__=='__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
     train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
+"""
